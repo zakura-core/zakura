valargroup/zebra

feat(network): verify Zakura header-sync commitment roots before persisting

zebra-network/src/zakura/header_sync/events.rs
@@ -1,3 +1,9 @@
+use std::sync::Arc;
+
+use zebra_chain::{
+    history_tree::HistoryTree, parallel::commitment_aux_verify::VerifiedHeaderCommitmentRoots,
+};
+
 use super::{config::*, error::*, validation::*, wire::*, *};
 use crate::zakura::{
     FrontierUpdate, HeaderSyncPeerSession, HeaderSyncServiceSummary, ServicePeerSnapshot,
@@ -26,6 +32,20 @@ pub struct HeaderSyncStartup {
     pub frontiers: HeaderSyncFrontiers,
     /// Durable best header tip loaded from storage at startup.
     pub best_header_tip: Option<(block::Height, block::Hash)>,
+    /// Hash of the durable best header tip's parent, if the tip is above genesis.
+    pub best_header_parent_hash: Option<block::Hash>,
+    /// The last checkpoint height — the VCT fast-sync handoff boundary.
+    ///
+    /// Header sync requests, verifies, persists, and tree-tracks peer-supplied commitment roots only
+    /// while the header frontier is below this height (the only region a VCT consumer reads them).
+    /// At/above it the frontier tree is dropped and header sync runs plain. Defaults to
+    /// `network.checkpoint_list().max_height()` (the height the embedded VCT frontier is pinned to).
+    pub last_checkpoint_height: block::Height,
+    /// History tree positioned at the durable best header tip.
+    ///
+    /// Pre-Heartwood or empty state uses the default (empty) tree; post-Heartwood this is the tree
+    /// reconstructed from durable roots so header-sync root verification can start immediately.
+    pub best_header_history_tree: Arc<HistoryTree>,
     /// Shared sync exchange frontier stream.
     pub frontier_updates: Option<watch::Receiver<FrontierUpdate>>,
     /// Local stream-5 advertisement.
@@ -56,11 +76,15 @@ impl HeaderSyncStartup {
         config: ZakuraHeaderSyncConfig,
         max_frame_bytes: u32,
     ) -> Self {
+        let last_checkpoint_height = network.checkpoint_list().max_height();
         Self {
             network,
             anchor,
             frontiers,
             best_header_tip,
+            best_header_parent_hash: None,
+            last_checkpoint_height,
+            best_header_history_tree: Arc::new(HistoryTree::default()),
             frontier_updates: None,
             config,
             max_frame_bytes,
@@ -217,6 +241,21 @@ pub enum HeaderSyncEvent {
     },
     /// State finalized or verified-body frontiers changed.
     StateFrontiersChanged(HeaderSyncFrontiers),
+    /// State returned the history tree rebuilt for the current best header tip.
+    ///
+    /// Answers a [`HeaderSyncAction::QueryBestHeaderHistoryTree`] — the single lazy rebuild triggered
+    /// when a below-checkpoint forward range finds the header-frontier tree behind the committed tip
+    /// (a non-Zakura path committed ahead). The tree is repositioned from durable state.
+    ///
+    /// Always sent once per query — including on a state read error — so the reactor's in-flight
+    /// rebuild guard is always cleared. `history_tree` is `None` when the rebuild failed (the guard is
+    /// cleared and the next forward range re-triggers it), `Some` on success.
+    BestHeaderHistoryTreeLoaded {
+        /// Best header tip the tree is positioned at when this reload was requested.
+        best_header_tip: block::Height,
+        /// History tree reconstructed by state, positioned at `best_header_tip`; `None` on failure.
+        history_tree: Option<Arc<HistoryTree>>,
+    },
     /// State successfully committed a header range.
     HeaderRangeCommitted {
         /// First committed height.
@@ -225,6 +264,8 @@ pub enum HeaderSyncEvent {
         tip_height: block::Height,
         /// New best header tip hash.
         tip_hash: block::Hash,
+        /// Hash of the new best header tip's parent, if known.
+        tip_parent_hash: Option<block::Hash>,
     },
     /// State rejected a previously requested range.
     HeaderRangeCommitFailed {
@@ -290,13 +331,28 @@ pub enum HeaderSyncAction {
         headers: Vec<Arc<block::Header>>,
         /// Advisory serialized body sizes, parallel to `headers`.
         body_sizes: Vec<u32>,
-        /// Per-height commitment roots, parallel to `headers`.
-        tree_aux_roots: Vec<BlockCommitmentRoots>,
+        /// Header-layer verified commitment roots for the confirmed prefix of `headers`.
+        ///
+        /// `Some` only for below-checkpoint forward ranges (the confirmed prefix is persisted).
+        /// `None` for checkpoint-authenticated backward backfill ranges (they fold onto the previous
+        /// checkpoint's tree, not the forward frontier) and for above-checkpoint forward ranges (past
+        /// the VCT handoff boundary, where roots are neither requested nor needed). Both persist no
+        /// roots.
+        verified_roots: Option<Box<VerifiedHeaderCommitmentRoots>>,
         /// Whether the range is expected to be finalized by checkpoint policy.
         finalized: bool,
     },
-    /// Ask state for the durable best header tip.
-    QueryBestHeaderTip,
+    /// Ask state to rebuild the header-frontier history tree at the current best header tip.
+    ///
+    /// The single reload trigger: dispatched when a below-checkpoint forward range finds the tree
+    /// behind the committed tip. State reconstructs from durable roots; `verified_block_tip` is the
+    /// reconstruction base.
+    QueryBestHeaderHistoryTree {
+        /// Verified block tip that is the reconstruction base.
+        verified_block_tip: block::Height,
+        /// Header tip the rebuilt tree must be positioned at.
+        best_header_tip: block::Height,
+    },
     /// Ask state for a bounded contiguous range of headers.
     QueryHeadersByHeightRange {
         /// Peer that requested the range.

zebra-network/src/zakura/header_sync/validation.rs
@@ -1,4 +1,8 @@
 use super::{error::*, events::*, wire::*, *};
+use zebra_chain::{
+    history_tree::HistoryTree,
+    parallel::commitment_aux_verify::{self, SuppliedRootsError, VerifiedHeaderCommitmentRoots},
+};
 
 pub(super) fn validate_anchor(
     network: &Network,
@@ -191,6 +195,37 @@ pub fn validate_header_range_links(
     }
 
     validate_internal_continuity(headers)
+}
+
+/// Verify supplied roots against the header commitments using `parent_history_tree`.
+pub fn validate_header_aux_commitments(
+    network: &Network,
+    parent_history_tree: &HistoryTree,
+    headers: &[Arc<block::Header>],
+    tree_aux_roots: &[BlockCommitmentRoots],
+) -> Result<VerifiedHeaderCommitmentRoots, HeaderSyncWireError> {
+    validate_tree_aux_roots_len(headers.len(), tree_aux_roots.len())?;
+
+    let items = headers
+        .iter()
+        .zip(tree_aux_roots.iter())
+        .map(|(header, roots)| (header.as_ref(), roots));
+
+    commitment_aux_verify::verify_supplied_roots_from_parts(
+        network,
+        parent_history_tree.clone(),
+        items,
+    )
+    .map_err(|(_height, error)| supplied_roots_error_into_wire(error))
+}
+
+fn supplied_roots_error_into_wire(error: SuppliedRootsError) -> HeaderSyncWireError {
+    match error {
+        SuppliedRootsError::InvalidHeaderCommitment(error) => {
+            HeaderSyncWireError::InvalidHeaderCommitment(error)
+        }
+        SuppliedRootsError::HistoryTree(error) => HeaderSyncWireError::HistoryTree(error),
+    }
 }
 
 /// Run all context-free validation checks for an inbound full-block tip flood.

zebra-state/src/service/finalized_state/zebra_db/block/tests/vectors.rs
@@ -625,6 +625,10 @@ fn header_range_roots_do_not_overwrite_committed_serving_index_rows() {
         false,
     );
 
+    let block2 = zebra_test::vectors::BLOCK_MAINNET_2_BYTES
+        .zcash_deserialize_into::<Arc<Block>>()
+        .expect("block 2 deserializes");
+
     write_full_block(&mut state, genesis.clone());
     write_full_block(&mut state, block1.clone());
 
@@ -637,7 +641,9 @@ fn header_range_roots_do_not_overwrite_committed_serving_index_rows() {
 
     // Re-deliver the real header for the committed height, but with garbage roots —
     // exactly what a malicious serving peer can put in a `Headers` response, since
-    // root bytes are unauthenticated at header-commit time.
+    // root bytes are unauthenticated at header-commit time. The committed height must be a confirmed
+    // (non-tip) root of the range, so re-deliver it as the first header of a two-header range whose
+    // confirmed prefix is exactly the poisoned height-1 root.
     let mut poisoned = root_at(Height(1));
     poisoned.sapling_tx = 99;
     poisoned.auth_data_root = zebra_chain::block::merkle::AuthDataRoot::from([0xAA; 32]);
@@ -647,8 +653,8 @@ fn header_range_roots_do_not_overwrite_committed_serving_index_rows() {
         .prepare_header_range_batch_with_roots(
             &state,
             genesis.hash(),
-            std::slice::from_ref(&block1.header),
-            &[0],
+            &[block1.header.clone(), block2.header.clone()],
+            &[0, 0],
             &[poisoned],
         )
         .expect("re-delivering the same header over a committed height is accepted");
@@ -661,6 +667,71 @@ fn header_range_roots_do_not_overwrite_committed_serving_index_rows() {
         verified_rows,
         "peer-supplied roots must not overwrite the verified committed row"
     );
+}
+
+/// A header-range commit persists exactly the confirmed prefix of roots it is given: the range
+/// tip's root is unauthenticated (only confirmed once the next range delivers its successor header),
+/// so the caller omits it and the state must not fabricate or persist one. A tip root left on disk
+/// would be folded, unverified, into the startup history-tree reconstruction.
+#[test]
+fn header_range_commit_persists_only_the_confirmed_root_prefix() {
+    let _init_guard = zebra_test::init();
+    let (state, genesis, block1) = mainnet_state_with_genesis();
+    let block2 = mainnet_block(2);
+
+    let headers = vec![block1.header.clone(), block2.header.clone()];
+    // Confirmed prefix: one root for the two-header range. The tip (height 2) root is omitted.
+    let confirmed_roots = vec![root_at(Height(1))];
+
+    let mut batch = DiskWriteBatch::new();
+    batch
+        .prepare_header_range_batch_with_roots(
+            &state,
+            genesis.hash(),
+            &headers,
+            &[0, 0],
+            &confirmed_roots,
+        )
+        .expect("a confirmed prefix one shorter than the headers is accepted");
+    state
+        .write_batch(batch)
+        .expect("header range batch writes successfully");
+
+    // Both headers are durable and the header tip advances to the range tip...
+    assert_eq!(state.best_header_tip(), Some((Height(2), block2.hash())));
+    // ...but only the confirmed height has a persisted root; the tip root is absent.
+    assert_eq!(
+        state.zakura_header_commitment_roots_by_height_range(Height(1)..=Height(1)),
+        vec![root_at(Height(1))],
+        "the confirmed height keeps its provisional root",
+    );
+    assert!(
+        state
+            .zakura_header_commitment_roots_by_height_range(Height(2)..=Height(2))
+            .is_empty(),
+        "the unconfirmed range tip must not have a persisted root",
+    );
+
+    // An empty vector is the checkpoint-authenticated backfill shape: it is accepted and persists no
+    // provisional roots, so the trust boundary is never crossed.
+    let mut batch = DiskWriteBatch::new();
+    batch
+        .prepare_header_range_batch_with_roots(&state, genesis.hash(), &headers, &[0, 0], &[])
+        .expect("an empty root vector is accepted for checkpoint-authenticated backfill");
+
+    // A full-length vector would include the unauthenticated tip root, so it is rejected.
+    let mut batch = DiskWriteBatch::new();
+    let full_roots = vec![root_at(Height(1)), root_at(Height(2))];
+    assert!(matches!(
+        batch.prepare_header_range_batch_with_roots(
+            &state,
+            genesis.hash(),
+            &headers,
+            &[0, 0],
+            &full_roots,
+        ),
+        Err(CommitHeaderRangeError::TreeAuxRootCountMismatch { .. }),
+    ));
 }
 
 /// Pruning-readiness guard: a committed height whose body is removed (as online

zakura-core/zakura

zakura-network/src/zakura/header_sync/events.rs
@@ -1,3 +1,9 @@
+use std::sync::Arc;
+
+use zakura_chain::{
+    history_tree::HistoryTree, parallel::commitment_aux_verify::VerifiedHeaderCommitmentRoots,
+};
+
 use super::{config::*, error::*, validation::*, wire::*, *};
 use crate::zakura::{
     FrontierUpdate, HeaderSyncPeerSession, HeaderSyncServiceSummary, ServicePeerSnapshot,
@@ -26,6 +32,20 @@ pub struct HeaderSyncStartup {
     pub frontiers: HeaderSyncFrontiers,
     /// Durable best header tip loaded from storage at startup.
     pub best_header_tip: Option<(block::Height, block::Hash)>,
+    /// Hash of the durable best header tip's parent, if the tip is above genesis.
+    pub best_header_parent_hash: Option<block::Hash>,
+    /// The last checkpoint height — the VCT fast-sync handoff boundary.
+    ///
+    /// Header sync requests, verifies, persists, and tree-tracks peer-supplied commitment roots only
+    /// while the header frontier is below this height (the only region a VCT consumer reads them).
+    /// At/above it the frontier tree is dropped and header sync runs plain. Defaults to
+    /// `network.checkpoint_list().max_height()` (the height the embedded VCT frontier is pinned to).
+    pub last_checkpoint_height: block::Height,
+    /// History tree positioned at the durable best header tip.
+    ///
+    /// Pre-Heartwood or empty state uses the default (empty) tree; post-Heartwood this is the tree
+    /// reconstructed from durable roots so header-sync root verification can start immediately.
+    pub best_header_history_tree: Arc<HistoryTree>,
     /// Shared sync exchange frontier stream.
     pub frontier_updates: Option<watch::Receiver<FrontierUpdate>>,
     /// Local header-sync v6 advertisement.
@@ -56,11 +76,15 @@ impl HeaderSyncStartup {
         config: ZakuraHeaderSyncConfig,
         max_frame_bytes: u32,
     ) -> Self {
+        let last_checkpoint_height = network.checkpoint_list().max_height();
         Self {
             network,
             anchor,
             frontiers,
             best_header_tip,
+            best_header_parent_hash: None,
+            last_checkpoint_height,
+            best_header_history_tree: Arc::new(HistoryTree::default()),
             frontier_updates: None,
             config,
             max_frame_bytes,
@@ -231,6 +255,21 @@ pub enum HeaderSyncEvent {
     },
     /// State finalized or verified-body frontiers changed.
     StateFrontiersChanged(HeaderSyncFrontiers),
+    /// State returned the history tree rebuilt for the current best header tip.
+    ///
+    /// Answers a [`HeaderSyncAction::QueryBestHeaderHistoryTree`] — the single lazy rebuild triggered
+    /// when a below-checkpoint forward range finds the header-frontier tree behind the committed tip
+    /// (a non-Zakura path committed ahead). The tree is repositioned from durable state.
+    ///
+    /// Always sent once per query — including on a state read error — so the reactor's in-flight
+    /// rebuild guard is always cleared. `history_tree` is `None` when the rebuild failed (the guard is
+    /// cleared and the next forward range re-triggers it), `Some` on success.
+    BestHeaderHistoryTreeLoaded {
+        /// Best header tip the tree is positioned at when this reload was requested.
+        best_header_tip: block::Height,
+        /// History tree reconstructed by state, positioned at `best_header_tip`; `None` on failure.
+        history_tree: Option<Arc<HistoryTree>>,
+    },
     /// State successfully committed a header range.
     HeaderRangeCommitted {
         /// First committed height.
@@ -239,6 +278,8 @@ pub enum HeaderSyncEvent {
         tip_height: block::Height,
         /// New best header tip hash.
         tip_hash: block::Hash,
+        /// Hash of the new best header tip's parent, if known.
+        tip_parent_hash: Option<block::Hash>,
     },
     /// State rejected a previously requested range.
     HeaderRangeCommitFailed {
@@ -328,13 +369,28 @@ pub enum HeaderSyncAction {
         headers: Vec<Arc<block::Header>>,
         /// Advisory serialized body sizes, parallel to `headers`.
         body_sizes: Vec<u32>,
-        /// Per-height commitment roots, parallel to `headers`.
-        tree_aux_roots: Vec<BlockCommitmentRoots>,
+        /// Header-layer verified commitment roots for the confirmed prefix of `headers`.
+        ///
+        /// `Some` only for below-checkpoint forward ranges (the confirmed prefix is persisted).
+        /// `None` for checkpoint-authenticated backward backfill ranges (they fold onto the previous
+        /// checkpoint's tree, not the forward frontier) and for above-checkpoint forward ranges (past
+        /// the VCT handoff boundary, where roots are neither requested nor needed). Both persist no
+        /// roots.
+        verified_roots: Option<Box<VerifiedHeaderCommitmentRoots>>,
         /// Whether the range is expected to be finalized by checkpoint policy.
         finalized: bool,
     },
-    /// Ask state for the durable best header tip.
-    QueryBestHeaderTip,
+    /// Ask state to rebuild the header-frontier history tree at the current best header tip.
+    ///
+    /// The single reload trigger: dispatched when a below-checkpoint forward range finds the tree
+    /// behind the committed tip. State reconstructs from durable roots; `verified_block_tip` is the
+    /// reconstruction base.
+    QueryBestHeaderHistoryTree {
+        /// Verified block tip that is the reconstruction base.
+        verified_block_tip: block::Height,
+        /// Header tip the rebuilt tree must be positioned at.
+        best_header_tip: block::Height,
+    },
     /// Ask state for a bounded contiguous range of headers.
     QueryHeadersByHeightRange {
         /// Peer that requested the range.

zakura-network/src/zakura/header_sync/validation.rs
@@ -1,4 +1,8 @@
 use super::{error::*, events::*, wire::*, *};
+use zakura_chain::{
+    history_tree::HistoryTree,
+    parallel::commitment_aux_verify::{self, SuppliedRootsError, VerifiedHeaderCommitmentRoots},
+};
 
 pub(super) fn validate_anchor(
     network: &Network,
@@ -191,6 +195,37 @@ pub fn validate_header_range_links(
     }
 
     validate_internal_continuity(headers)
+}
+
+/// Verify supplied roots against the header commitments using `parent_history_tree`.
+pub fn validate_header_aux_commitments(
+    network: &Network,
+    parent_history_tree: &HistoryTree,
+    headers: &[Arc<block::Header>],
+    tree_aux_roots: &[BlockCommitmentRoots],
+) -> Result<VerifiedHeaderCommitmentRoots, HeaderSyncWireError> {
+    validate_tree_aux_roots_len(headers.len(), tree_aux_roots.len())?;
+
+    let items = headers
+        .iter()
+        .zip(tree_aux_roots.iter())
+        .map(|(header, roots)| (header.as_ref(), roots));
+
+    commitment_aux_verify::verify_supplied_roots_from_parts(
+        network,
+        parent_history_tree.clone(),
+        items,
+    )
+    .map_err(|(_height, error)| supplied_roots_error_into_wire(error))
+}
+
+fn supplied_roots_error_into_wire(error: SuppliedRootsError) -> HeaderSyncWireError {
+    match error {
+        SuppliedRootsError::InvalidHeaderCommitment(error) => {
+            HeaderSyncWireError::InvalidHeaderCommitment(error)
+        }
+        SuppliedRootsError::HistoryTree(error) => HeaderSyncWireError::HistoryTree(error),
+    }
 }
 
 /// Run all context-free validation checks for an inbound full-block tip flood.

zakura-state/src/service/finalized_state/zakura_db/block/tests/vectors.rs
@@ -629,6 +629,10 @@ fn header_range_roots_do_not_overwrite_committed_serving_index_rows() {
         false,
     );
 
+    let block2 = zakura_test::vectors::BLOCK_MAINNET_2_BYTES
+        .zcash_deserialize_into::<Arc<Block>>()
+        .expect("block 2 deserializes");
+
     write_full_block(&mut state, genesis.clone());
     write_full_block(&mut state, block1.clone());
 
@@ -641,7 +645,9 @@ fn header_range_roots_do_not_overwrite_committed_serving_index_rows() {
 
     // Re-deliver the real header for the committed height, but with garbage roots —
     // exactly what a malicious serving peer can put in a `Headers` response, since
-    // root bytes are unauthenticated at header-commit time.
+    // root bytes are unauthenticated at header-commit time. The committed height must be a confirmed
+    // (non-tip) root of the range, so re-deliver it as the first header of a two-header range whose
+    // confirmed prefix is exactly the poisoned height-1 root.
     let mut poisoned = root_at(Height(1));
     poisoned.sapling_tx = 99;
     poisoned.auth_data_root = zakura_chain::block::merkle::AuthDataRoot::from([0xAA; 32]);
@@ -651,8 +657,8 @@ fn header_range_roots_do_not_overwrite_committed_serving_index_rows() {
         .prepare_header_range_batch_with_roots(
             &state,
             genesis.hash(),
-            std::slice::from_ref(&block1.header),
-            &[0],
+            &[block1.header.clone(), block2.header.clone()],
+            &[0, 0],
             &[poisoned],
         )
         .expect("re-delivering the same header over a committed height is accepted");
@@ -665,6 +671,71 @@ fn header_range_roots_do_not_overwrite_committed_serving_index_rows() {
         verified_rows,
         "peer-supplied roots must not overwrite the verified committed row"
     );
+}
+
+/// A header-range commit persists exactly the confirmed prefix of roots it is given: the range
+/// tip's root is unauthenticated (only confirmed once the next range delivers its successor header),
+/// so the caller omits it and the state must not fabricate or persist one. A tip root left on disk
+/// would be folded, unverified, into the startup history-tree reconstruction.
+#[test]
+fn header_range_commit_persists_only_the_confirmed_root_prefix() {
+    let _init_guard = zakura_test::init();
+    let (state, genesis, block1) = mainnet_state_with_genesis();
+    let block2 = mainnet_block(2);
+
+    let headers = vec![block1.header.clone(), block2.header.clone()];
+    // Confirmed prefix: one root for the two-header range. The tip (height 2) root is omitted.
+    let confirmed_roots = vec![root_at(Height(1))];
+
+    let mut batch = DiskWriteBatch::new();
+    batch
+        .prepare_header_range_batch_with_roots(
+            &state,
+            genesis.hash(),
+            &headers,
+            &[0, 0],
+            &confirmed_roots,
+        )
+        .expect("a confirmed prefix one shorter than the headers is accepted");
+    state
+        .write_batch(batch)
+        .expect("header range batch writes successfully");
+
+    // Both headers are durable and the header tip advances to the range tip...
+    assert_eq!(state.best_header_tip(), Some((Height(2), block2.hash())));
+    // ...but only the confirmed height has a persisted root; the tip root is absent.
+    assert_eq!(
+        state.zakura_header_commitment_roots_by_height_range(Height(1)..=Height(1)),
+        vec![root_at(Height(1))],
+        "the confirmed height keeps its provisional root",
+    );
+    assert!(
+        state
+            .zakura_header_commitment_roots_by_height_range(Height(2)..=Height(2))
+            .is_empty(),
+        "the unconfirmed range tip must not have a persisted root",
+    );
+
+    // An empty vector is the checkpoint-authenticated backfill shape: it is accepted and persists no
+    // provisional roots, so the trust boundary is never crossed.
+    let mut batch = DiskWriteBatch::new();
+    batch
+        .prepare_header_range_batch_with_roots(&state, genesis.hash(), &headers, &[0, 0], &[])
+        .expect("an empty root vector is accepted for checkpoint-authenticated backfill");
+
+    // A full-length vector would include the unauthenticated tip root, so it is rejected.
+    let mut batch = DiskWriteBatch::new();
+    let full_roots = vec![root_at(Height(1)), root_at(Height(2))];
+    assert!(matches!(
+        batch.prepare_header_range_batch_with_roots(
+            &state,
+            genesis.hash(),
+            &headers,
+            &[0, 0],
+            &full_roots,
+        ),
+        Err(CommitHeaderRangeError::TreeAuxRootCountMismatch { .. }),
+    ));
 }
 
 /// Pruning-readiness guard: a committed height whose body is removed (as online